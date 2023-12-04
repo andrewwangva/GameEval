61504612d591b1d69737ebba559744cd65874b8a
Amogh Bhatnagar <amoghb@mit.edu>
2023-12-03 20:23:59 -0500

    semi finished puzzles

--- scraper.py
+++ scraper.py
@@ -1,11 +1,17 @@
 import os
-import pandas 
-import numpy
+import pandas as pd
+import numpy as np
 import requests
+from concurrent.futures import ThreadPoolExecutor
+import pickle
+from tqdm import tqdm
+tqdm.pandas()
 
-ex_urls = ['https://lichess.org/yyznGmXs/black#34', 'https://lichess.org/gyFeQsOE#35']
+
+#ex_urls = ['https://lichess.org/yyznGmXs/black#34', 'https://lichess.org/gyFeQsOE#35']
 
 #put in function to paralleize on pandas
+
 
 def get_png(ex_url):
     game_id = ex_url.split('/')[3]
@@ -16,9 +22,33 @@ def get_png(ex_url):
     download_url = f'https://lichess.org/game/export/{game_id}?evals=0&clocks=0'
     response = requests.get(download_url)
     text_str = str(response.content, 'utf-8')
-    pgn = text_str.split('\n')[-4]
+    new_line_split = text_str.split('\n')
+    date = new_line_split[2]
+    if '2022' in date or '2023' in date:
+        return text_str.split('\n')[-4]
+    else:
+        return np.nan
+nrows = 50000
 
-    return pgn 
-    
+batch_size = 1000
+for i in range(11, nrows//batch_size):
+    try:
+        puzzles = pd.read_csv('lichess_db_puzzle.csv', skiprows= i* batch_size+1, nrows = batch_size, names = ["PuzzleId","FEN","Moves","Rating","RatingDeviation","Popularity","NbPlays","Themes","GameUrl","OpeningTags"])
+        puzzles['pgn'] = puzzles['GameUrl'].progress_apply(get_png)
+
+        puzzles = puzzles[puzzles['pgn'].notna()]
+        puzzles.to_json(f'puzzles_annot_{i}.json', orient = 'records', lines=True)
+    except Exception as e:
+        print(f'{i}', 'didn\'t work')
+
+
+
+
+
+
+
+
+
+
 
   
